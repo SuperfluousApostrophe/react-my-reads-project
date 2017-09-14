60448b0b68e91f160488b9769ddb8bddf25b7afa
meh252 <meh252@104.39.255.90>
2017-09-14 10:42:42 -0400

    Converted the bookshelf to a series of components
    New components include Bookshelf, Book, & BookActions - each represents a reusable piece of the original HTML mockup.
    Test data is working - displaying stub content via components.

---
+++ src/Bookshelf.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import Book from './Book.js'
+
+class Bookshelf extends Component{
+   shelves = new Map([
+      ['currReading',{title:'Currently Reading'}],
+      ['wantRead',{title:'Want To Read'}],
+      ['read',{title:'Read'}]
+   ]);
+   render(){
+      const {shelfName} = this.props;
+      let shelf = this.shelves.get(shelfName);
+      let booksToShow = 10;
+      
+      let Booklist = [];
+      for(let i=0;i<booksToShow;i++){
+        Booklist.push(<li key={i}><Book bookObj=""/></li>);
+      }
+      
+      
+      return(
+         <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf.title}</h2>
+            <div className="bookshelf-books">
+               <ol className="books-grid">
+                  {Booklist}
+              </ol>
+            </div>
+          </div>
+      
+      
+      );
+   };
+   
+};
+export default Bookshelf
