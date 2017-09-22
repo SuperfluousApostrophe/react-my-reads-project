e1366f17d8e2b534230091447e1c087e848d3e10
meh252 <meh252@104.39.255.90>
2017-09-22 11:57:13 -0400

    Refactored action list to allow for additional options in the future

--- src/BookActions.js
+++ src/BookActions.js
@@ -1,22 +1,52 @@
 import React, {Component} from 'react';
-import * as BooksAPI from './BooksAPI';
 
 class BookActions extends Component{
+      options = new Map([
+         ["moveTo",{
+            isDisabled:true,
+            displayValue:'Move to...'
+         }],
+         ["currentlyReading",{
+               isDisabled:false,
+               displayValue:'Currently Reading'
+            }],
+         ["wantToRead",{
+               isDisabled:false,
+               displayValue:'Want to Read'
+            }],
+         ["read",{
+               isDisabled:false,
+               displayValue:'Read'
+            }],
+         ["none",{
+               isDisabled:false,
+               displayValue:'None'
+            }],
+      ]);
+      buildOptions = function(){
+         let optionsElements = [];
+         for(let [key, value] of this.options){
+            optionsElements.push(<option 
+              key={key} 
+               value={key} 
+              disabled={value.isDisabled} 
+              >{value.displayValue}</option>);
+         }
+      return optionsElements;
+   };
+   
    render(){
       const {bookObj, updateShelf} = this.props;
       let tmpBookObj = bookObj;
       if(tmpBookObj.shelf === undefined){
          tmpBookObj.shelf = 'none';
       }
-      
       return(
          <div className="book-shelf-changer">
             <select onChange={(event)=>updateShelf(event, bookObj)} value={tmpBookObj.shelf}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
+            {
+              this.buildOptions()
+            }  
             </select>
           </div>
       );
